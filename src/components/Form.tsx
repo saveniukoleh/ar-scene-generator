245bacde7275a4b9b7b23eff1c615de571d9f768
Oleg Saveniuk <oleg.saveniuk@gmail.com>
2021-03-17 10:02:42 +0200

    Update theme

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
 class Form extends React.Component {
+    state = {
+        value: '',
+    }
+
+    constructor(props: any) {
+        super(props);
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event: any) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleSubmit(event: any) {
+        event.preventDefault();
+        if (!this.state.value) return;
+        console.log(this.state.value);
+    }
 
     render() {
         return <div className="form">
             <div className='form-group'>
                 <label className="form-label-pattern" htmlFor="inputLarge">Enter the number of your patterns</label>
-                <input className="form-control form-control-lg" type="text" id="inputLarge"></input>
+                <input className="form-control form-control-lg" type="text" id="inputLarge" onChange={this.handleChange}></input>
             </div>
+            <button className="btn btn-primary btn-lg" onClick={this.handleSubmit}>Submit</button>
         </div>
     }
 }
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,7 +4,7 @@ class ProgressBar extends React.Component {
 
     render() {
         return <div className="progress">
-            <div className="progress-bar bg-info progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "50%"}} aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
+            <div className="progress-bar bg-primary progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "50%"}} aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
         </div>
     }
 }
