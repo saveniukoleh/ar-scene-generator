8f9ebbb0a41de214089d22198f8e60814b460923
Oleg Saveniuk <oleg.saveniuk@gmail.com>
2021-03-24 23:32:35 +0200

    Add file input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,13 +22,20 @@ class Form extends React.Component {
         console.log(this.state.value);
     }
 
+    onChange(e: any) {
+        e.target.files[0] && console.log(e.target.files[0].name);
+    }
+
     render() {
         return <div className="form">
             <div className='form-group'>
                 <label className="form-label-pattern" htmlFor="inputLarge">Enter the number of your patterns</label>
                 <input className="form-control form-control-lg" type="text" id="inputLarge" onChange={this.handleChange}></input>
             </div>
+            
             <button className="btn btn-primary btn-lg" onClick={this.handleSubmit}>Submit</button>
+            
+            <input type="file" style={{paddingTop: '15px'}} className="form-control-file" id="exampleInputFile" aria-describedby="fileHelp" onChange={this.onChange}></input>
         </div>
     }
 }
