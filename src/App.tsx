bd6231e26ae496fa118079df35b0c9ba2d72f3b9
Oleg Saveniuk <oleg.saveniuk@gmail.com>
2021-03-25 21:05:10 +0200

    Add some text

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './css/bootstrap.css';
 import './css/main.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Form from './components/Form';
 import ProgressBar from './components/ProgressBar';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
     return <div className="App">
       <Header />
       <Form />
+      <Footer />
       <ProgressBar />
     </div>;
   }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SOME_TEXT} from '../ts/config';
+
+class Footer extends React.Component {
+    render() {
+        return <div className='footer'>
+            <div className='alert alert-dismissible alert-danger'>
+                {SOME_TEXT}
+            </div>
+        </div>;
+    }
+}
+
+export default Footer;
