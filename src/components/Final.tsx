0ae1cddc79d31865a4cfcb154a8c88e7dcc497d8
Oleg Saveniuk <oleg.saveniuk@gmail.com>
2021-04-06 22:10:42 +0300

    Remove test

--- src/components/Final.tsx
+++ src/components/Final.tsx
@@ -3,7 +3,7 @@ import inputManager from "../ts/InputManager";
 export default function Final() {
   return (
     <div className="final-output">
-      <b>Згенерований код для вашої програмиbb:</b>
+      <b>Згенерований код для вашої програми:</b>
       <br></br>
       <br></br>
       <div style={{ whiteSpace: "pre" }}>{inputManager.compileData()}</div>
